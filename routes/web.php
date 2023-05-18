35e1192b47d8d2fd465812915bf4c27218741097
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-18 22:54:55 +0530

    auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EmployeeController::class, 'index']);
@@ -9,3 +10,9 @@ Route::get('/fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll
 Route::delete('/delete', [EmployeeController::class, 'delete'])->name('delete');
 Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
 Route::post('/update', [EmployeeController::class, 'update'])->name('update');
+
+
+
+Route::get('/auth', [AuthController::class, 'register']);
+Route::post('/add_account', [AuthController::class, 'add_account'])->name('add_account');
+Route::post('/login_account', [AuthController::class, 'login_account'])->name('login_account');
