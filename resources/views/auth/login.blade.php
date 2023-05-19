396b58fb86ad708f2e815aa2854b658de1561ecc
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-19 13:49:11 +0530

    bootstrap files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD App Laravel 8 & Ajax</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css' />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css" />
+
+
 </head>
 
 <body>
@@ -42,6 +48,7 @@
                                     <button type="submit" class="btn btn-dark btn-block">Signin</button>
                                 </div>
                             </form>
+                            <a class="nav-link" href="{{ route('register-user') }}">Register Here</a>
                         </div>
                     </div>
                 </div>
