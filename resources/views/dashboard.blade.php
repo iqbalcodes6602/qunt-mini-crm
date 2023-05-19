e47e0d9dc7cc40b093a688d636b5151081e58abe
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-19 13:16:17 +0530

    dashboard common

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,117 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css" />
 
+    <style>
+        .nav-tabs .nav-link {
+            color: #333;
+            /* Set the text color for non-active tabs */
+            background-color: #f8f9fa;
+            /* Set the background color for non-active tabs */
+            border-color: #ddd;
+            /* Set the border color for non-active tabs */
+        }
+
+        .nav-tabs .nav-link.active {
+            color: #fff;
+            /* Set the text color for the active tab */
+            background-color: #007bff;
+            /* Set the background color for the active tab */
+            border-color: #007bff;
+            /* Set the border color for the active tab */
+        }
+    </style>
 </head>
+
+{{-- add new company modal start --}}
+<div class="modal fade" id="addCompanyModal" tabindex="-1" aria-labelledby="exampleModalLabel" data-bs-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add New company</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="#" method="POST" id="add_company_form" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body p-4 bg-light">
+                    <div class="my-2">
+                        <label for="fname">Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Name of Company" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="website">Website</label>
+                        <input type="text" name="website" class="form-control" placeholder="Website" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="City">City</label>
+                        <input type="text" name="city" class="form-control" placeholder="Location of Company" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="logo">Select Logo</label>
+                        <input type="file" name="logo" class="form-control" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" id="add_company_btn" class="btn btn-primary">Add company</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{-- add new company modal end --}}
+
+{{-- edit company modal start --}}
+<div class="modal fade" id="editCompanyModal" tabindex="-1" aria-labelledby="exampleModalLabel" data-bs-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit company</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="#" method="POST" id="edit_company_form" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="comp_id" id="comp_id">
+                <input type="hidden" name="comp_logo" id="comp_logo">
+                <div class="modal-body p-4 bg-light">
+                    <div class="my-2">
+                        <label for="fname">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Name of comp" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="website">Website</label>
+                        <input type="text" name="website" id="website" class="form-control" placeholder="Website" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="city">City</label>
+                        <input type="text" name="city" id="city" class="form-control" placeholder="Location of city" required>
+                    </div>
+                    <div class="my-2">
+                        <label for="logo">Select Logo</label>
+                        <input type="file" name="logo" class="form-control">
+                    </div>
+                    <div class="mt-2" id="logo">
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" id="edit_company_btn" class="btn btn-success">Update company</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{-- edit company modal end --}}
+
+
 
 
 {{-- add new employee modal start --}}
@@ -115,6 +225,10 @@
 </div>
 {{-- edit employee modal end --}}
 
+
+
+
+
 <body>
     <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
         <div class="container">
@@ -132,21 +246,65 @@
         </div>
     </nav>
     @yield('content')
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-lg-12">
-                <div class="card shadow">
-                    <div class="card-header bg-danger d-flex justify-content-between align-items-center">
-                        <h3 class="text-light">Manage Employees</h3>
-                        <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addEmployeeModal"><i class="bi-plus-circle me-2"></i>Add New Employee</button>
-                    </div>
-                    <div class="card-body" id="show_all_employees">
-                        <h1 class="text-center text-secondary my-5">Loading...</h1>
+
+
+    <!-- tabs-->
+    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Company</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Employee</button>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <!--company tab-->
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <div class="container">
+                <div class="row my-5">
+                    <div class="col-lg-12">
+                        <div class="card shadow">
+                            <div class="card-header bg-danger d-flex justify-content-between align-items-center">
+                                <h3 class="text-light">Manage Companies</h3>
+                                <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addCompanyModal"><i class="bi-plus-circle me-2"></i>Add New Comany</button>
+                            </div>
+                            <div class="card-body" id="show_all_companys">
+                                <h1 class="text-center text-secondary my-5">Loading...</h1>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!--company tab closed-->
+
+
+
+        <!-- employee tab -->
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="container">
+                <div class="row my-5">
+                    <div class="col-lg-12">
+                        <div class="card shadow">
+                            <div class="card-header bg-danger d-flex justify-content-between align-items-center">
+                                <h3 class="text-light">Manage Employees</h3>
+                                <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addEmployeeModal"><i class="bi-plus-circle me-2"></i>Add New Employee</button>
+                            </div>
+                            <div class="card-body" id="show_all_employees">
+                                <h1 class="text-center text-secondary my-5">Loading...</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- employee tab closed -->
     </div>
+    <!-- tabs -->
+
+
+
+
 </body>
 
 
@@ -155,6 +313,8 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js'></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- employee ajax -->
 <script>
     $(function() {
 
@@ -164,7 +324,7 @@
             const fd = new FormData(this);
             $("#add_employee_btn").text('Adding...');
             $.ajax({
-                url: '{{route("store")}}',
+                url: '{{route("emp_store")}}',
                 method: 'post',
                 data: fd,
                 cache: false,
@@ -192,7 +352,7 @@
             e.preventDefault();
             let id = $(this).attr('id');
             $.ajax({
-                url: '{{ route("edit") }}',
+                url: '{{ route("emp_edit") }}',
                 method: 'get',
                 data: {
                     id: id,
@@ -218,7 +378,7 @@
             const fd = new FormData(this);
             $("#edit_employee_btn").text('Updating...');
             $.ajax({
-                url: '{{ route("update") }}',
+                url: '{{ route("emp_update") }}',
                 method: 'post',
                 data: fd,
                 cache: false,
@@ -257,7 +417,7 @@
             }).then((result) => {
                 if (result.isConfirmed) {
                     $.ajax({
-                        url: '{{ route("delete") }}',
+                        url: '{{ route("emp_delete") }}',
                         method: 'delete',
                         data: {
                             id: id,
@@ -282,7 +442,7 @@
 
         function fetchAllEmployees() {
             $.ajax({
-                url: '{{ route("fetchAll") }}',
+                url: '{{ route("emp_fetchAll") }}',
                 method: 'get',
                 success: function(response) {
                     $("#show_all_employees").html(response);
@@ -294,6 +454,150 @@
         }
     });
 </script>
+<!-- employee ajax end-->
 
+
+
+<!-- company ajax -->
+<script>
+    $(function() {
+
+        // add new company ajax request
+        $("#add_company_form").submit(function(e) {
+            e.preventDefault();
+            const fd = new FormData(this);
+            $("#add_company_btn").text('Adding...');
+            $.ajax({
+                url: '{{route("comp_store")}}',
+                method: 'post',
+                data: fd,
+                cache: false,
+                contentType: false,
+                processData: false,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status == 200) {
+                        Swal.fire(
+                            'Added!',
+                            'company Added Successfully!',
+                            'success'
+                        )
+                        fetchAllCompanys();
+                    }
+                    $("#add_company_btn").text('Add company');
+                    $("#add_company_form")[0].reset();
+                    $("#addcompanyModal").modal('hide');
+                }
+            });
+        });
+
+        // edit company ajax request
+        $(document).on('click', '.editIcon', function(e) {
+            e.preventDefault();
+            let id = $(this).attr('id');
+            $.ajax({
+                url: '{{ route("comp_edit") }}',
+                method: 'get',
+                data: {
+                    id: id,
+                    _token: '{{ csrf_token() }}'
+                },
+                success: function(response) {
+                    $("#name").val(response.name);
+                    $("#lname").val(response.last_name);
+                    $("#email").val(response.email);
+                    $("#website").val(response.website);
+                    $("#city").val(response.city);
+                    $("#logo").html(
+                        `<img src="storage/images/${response.logo}" width="100" class="img-fluid img-thumbnail">`);
+                    $("#comp_id").val(response.id);
+                    $("#comp_logo").val(response.logo);
+                }
+            });
+        });
+
+        // update company ajax request
+        $("#edit_company_form").submit(function(e) {
+            e.preventDefault();
+            const fd = new FormData(this);
+            $("#edit_company_btn").text('Updating...');
+            $.ajax({
+                url: '{{ route("comp_update") }}',
+                method: 'post',
+                data: fd,
+                cache: false,
+                contentType: false,
+                processData: false,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status == 200) {
+                        Swal.fire(
+                            'Updated!',
+                            'company Updated Successfully!',
+                            'success'
+                        )
+                        fetchAllCompanys();
+                    }
+                    $("#edit_company_btn").text('Update company');
+                    $("#edit_company_form")[0].reset();
+                    $("#editcompanyModal").modal('hide');
+                }
+            });
+        });
+
+        // delete company ajax request
+        $(document).on('click', '.deleteIcon', function(e) {
+            e.preventDefault();
+            let id = $(this).attr('id');
+            let csrf = '{{ csrf_token() }}';
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                        url: '{{ route("comp_delete") }}',
+                        method: 'delete',
+                        data: {
+                            id: id,
+                            _token: csrf
+                        },
+                        success: function(response) {
+                            console.log(response);
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            fetchAllCompanys();
+                        }
+                    });
+                }
+            })
+        });
+
+        // fetch all companys ajax request
+        fetchAllCompanys();
+
+        function fetchAllCompanys() {
+            $.ajax({
+                url: '{{ route("comp_fetchAll") }}',
+                method: 'get',
+                success: function(response) {
+                    $("#show_all_companys").html(response);
+                    $("table").DataTable({
+                        order: [0, 'desc']
+                    });
+                }
+            });
+        }
+    });
+</script>
+<!-- company ajax end-->
 
 </html>
