a04c485262e5e72d83a427cc5c9116edd1d2f49d
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-19 08:58:20 +0530

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EmployeeController::class, 'index']);
 Route::post('/store', [EmployeeController::class, 'store'])->name('store');
@@ -16,3 +17,13 @@ Route::post('/update', [EmployeeController::class, 'update'])->name('update');
 Route::get('/auth', [AuthController::class, 'register']);
 Route::post('/add_account', [AuthController::class, 'add_account'])->name('add_account');
 Route::post('/login_account', [AuthController::class, 'login_account'])->name('login_account');
+
+
+
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
