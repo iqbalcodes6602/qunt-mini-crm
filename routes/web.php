e72f13067605262d6b24fc51a52f18d76443feb3
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-05-19 11:32:11 +0530

    routes updates

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AuthController;
+use Faker\Provider\ar_EG\Company;
 
-Route::get('/', [EmployeeController::class, 'index']);
-Route::post('/store', [EmployeeController::class, 'store'])->name('store');
-Route::get('/fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
-Route::delete('/delete', [EmployeeController::class, 'delete'])->name('delete');
-Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
-Route::post('/update', [EmployeeController::class, 'update'])->name('update');
+// Route::get('/', [EmployeeController::class, 'index']);
+Route::post('/emp_store', [EmployeeController::class, 'store'])->name('store');
+Route::get('/emp_fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
+Route::delete('/emp_delete', [EmployeeController::class, 'delete'])->name('delete');
+Route::get('/emp_edit', [EmployeeController::class, 'edit'])->name('edit');
+Route::post('/emp_update', [EmployeeController::class, 'update'])->name('update');
+
+
+
+
+Route::post('/comp_store', [CompanyController::class, 'store'])->name('store');
+Route::get('/emp_fetchall', [CompanyController::class, 'fetchAll'])->name('fetchAll');
+Route::delete('/comp_delete', [CompanyController::class, 'delete'])->name('delete');
+Route::get('/comp_edit', [CompanyController::class, 'edit'])->name('edit');
+Route::post('/comp_update', [CompanyController::class, 'update'])->name('update');
+
 
 
 
 
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('admin_dashboard', [CustomAuthController::class, 'admin_dashboard']); 
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
